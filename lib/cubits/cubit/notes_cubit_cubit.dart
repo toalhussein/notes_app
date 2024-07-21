722e986428b82f23972cb68a050531c228f6b6d4
toalhussein <toalhussein@gmail.com>
2024-07-21 12:05:36 +0300

    create fetch notes mehtod

--- lib/cubits/cubit/notes_cubit_cubit.dart
+++ lib/cubits/cubit/notes_cubit_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/models/note_model.dart';
+
+import '../../constans.dart';
 
 part 'notes_cubit_state.dart';
 
 class NotesCubitCubit extends Cubit<NotesCubitState> {
-  NotesCubitCubit() : super(NotesCubitInitial());
+  NotesCubitCubit() : super(NotesInitial());
+
+  fetchAllNotes() async {
+    try {
+      var notesBox = Hive.box<NoteModel>(KNotesBox);
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NotesSuccess(notes));
+    } catch (e) {
+      emit(NotesFailer(e.toString()));
+    }
+  }
 }
--- lib/cubits/cubit/notes_cubit_state.dart
+++ lib/cubits/cubit/notes_cubit_state.dart
@@ -3,15 +3,15 @@ part of 'notes_cubit_cubit.dart';
 @immutable
 sealed class NotesCubitState {}
 
-final class NotesCubitInitial extends NotesCubitState {}
-final class NotesCubitLoading extends NotesCubitState {}
-final class NotesCubitSuccess extends NotesCubitState {
+final class NotesInitial extends NotesCubitState {}
+final class NotesLoading extends NotesCubitState {}
+final class NotesSuccess extends NotesCubitState {
   final List<NoteModel> notes;
 
-  NotesCubitSuccess(this.notes);
+  NotesSuccess(this.notes);
 }
-final class NotesCubitFailer extends NotesCubitState {
+final class NotesFailer extends NotesCubitState {
   final String errMessage ;
 
-  NotesCubitFailer(this.errMessage);
+  NotesFailer(this.errMessage);
 }
