9c510b90edfc6bb1ea1de78a37b86c857a98995a
toalhussein <toalhussein@gmail.com>
2024-07-21 11:47:00 +0300

    fix bottom sheet errors

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -15,6 +15,7 @@ class NotesView extends StatelessWidget {
         backgroundColor: KPrimaryColor,
         onPressed: () {
           showModalBottomSheet(
+            isScrollControlled: true,
               context: context,
               builder: (context) {
                 return AddNoteSheet();
